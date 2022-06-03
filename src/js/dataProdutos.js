5257c6bb1c45201d4fda672532290245174db6fe
Leonardo Rodrigues <leonardorodriguesaai@outlook.com>
2022-06-03 16:56:03 -0300

    entrega s2-06 finalizado

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,37 +1,97 @@
 const produtos = [
     {
+        id: 1,
         nome: 'Banana',
-        preco: 2,
+        preco: '2.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: "./src/img/banana.png"
+        img: "./src/img/unsplash_0WW38q7lGZA.png",
+        promocao: true,
+        precoPromocao: '1.00',
+        componentes: [
+            'Potássio',
+            'Vitamina B6',
+            'Vitamina C',
+            'Folatos'
+        ] 
     },
     {
+        id: 2,
+        nome: 'Churros',
+        preco: '3.00',
+        secao: 'Panificadora',
+        categoria: 'doce',
+        img: "./src/img/churros.png",
+        promocao: true,
+        precoPromocao: '1.00',
+        componentes: [
+            'Doce de leite',
+            'Canela',
+            'Massa sequinha',
+        ] 
+    },
+    {
+        id: 3,
         nome: 'Morango',
-        preco: 2,
+        preco: '2.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/morango.png'
+        img: './src/img/unsplash_IK2ep3jMnHA.png',
+        promocao: false,
+        precoPromocao: '0',
+        componentes: [
+            'Fibras',
+            'Vitamina C',
+            'Cálcio',
+            'Ferro'
+        ] 
     },
     {
+        id: 4,
         nome: 'Maçã',
-        preco: 2,
+        preco: '2.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/maca.png'
+        img: './src/img/unsplash_wXuzS9xR49M.png',
+        promocao: true,
+        precoPromocao: '1.50',
+        componentes: [
+            'Potássio',
+            'Vitamina A',
+            'Vitamina E',
+            'Vitamina C'
+        ] 
     },
     {
+        id: 5,
         nome: 'Pão',
-        preco: 4,
+        preco: '4.00',
         secao: 'Panificadora',
         categoria: 'Pães',
-        img: './src/img/pao.png'
+        img: './src/img/unsplash__0IzI-gEH4w.png',
+        promocao: true,
+        precoPromocao: '2.50',
+        componentes: [
+            'Cálcio',
+            'Sódio',
+            'Fibra Alimentar',
+            'Proteínas'
+        ] 
     },
     {
+        id: 6,
         nome: 'Leite',
-        preco: 5,
-        secao: 'Laticínio',
+        preco: '5.00',
+        secao: 'Laticinio',
         categoria: 'Leite',
-        img: './src/img/leite.png'
-    }
+        img: './src/img/unsplash_rbJ9BY8bRRY.png',
+        promocao: false,
+        precoPromocao: '1.00',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ] 
+    },
+    
 ]
--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,16 @@ const botaoMostrarHortifruti = document.querySelector(".estiloGeralBotoes--filtr
 const botaoMostrarTodos = document.querySelector(".estiloGeralBotoes--mostrarTodos")
 const botaoBuscar = document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome")
 const precoTotal = document.querySelector("#precoTotal")
+const ulCarrinho = document.querySelector(".carrinho ul")
 
 function posicionaCard(produtos) {    
-    let precoContador = 0
+    
     produtos.forEach(produto => {
         const item = criaProduto(produto)
         ul.append(item)
-        precoContador += produto.preco
+        
     })
-    precoTotal.innerText = `R$ ${precoContador}.00`
+    
 }
 
 posicionaCard(produtos)
@@ -21,15 +22,35 @@ function criaProduto(item) {
     const nome = document.createElement("h3")
     const preco = document.createElement("p")
     const secao = document.createElement("span")
+    const componentes = componentesDoItem(item)
+    const addProduto = document.createElement("button")
+    
+    addProduto.className = `estiloGeralBotoes addProduto`
+    addProduto.id = item.id
 
     img.src = item.img
     nome.innerText = item.nome
-    preco.innerText = `R$ ${item.preco}.00`
     secao.innerText = item.secao
+    preco.innerText = `R$ ${parseInt(item.preco)}.00`
+    addProduto.innerText = "Comprar"
     
     const produto = document.createElement("li")
-    produto.append(img, nome, preco, secao)
+    const div = document.createElement("div")
+    div.className = "containerListaProdutos__div"
+    div.append(preco, addProduto)
+    produto.append(img, nome, secao, componentes, div)
     return produto
+}
+
+function componentesDoItem(item){
+    const componentes = document.createElement("span")
+    componentes.className = "componentes"
+    const componentsHTML = []
+    const listCompenents = item.componentes
+    
+    listCompenents.forEach((component, index) => componentsHTML.push(`${index+1}. ${component} <br>`))
+    componentes.innerHTML = componentsHTML.join("")
+    return componentes
 }
 
 
@@ -48,14 +69,79 @@ function filtrarPorNome(){
         const texto = input.value 
         const textoTratado = texto.toLowerCase()
         const produto = item.nome.toLowerCase()
+        const secao = item.secao.toLowerCase()
+        const categoria = item.categoria.toLowerCase()
         if (textoTratado == produto) {
             return produto
+        }
+        else if (textoTratado == secao) {
+            return secao
+        }
+        else if (textoTratado == categoria) {
+            return categoria
         }
     })
     ul.innerHTML = ""
     posicionaCard(filtroPorNome)
     input.value = ""
 }
+
+function criaCardCarrinho(item){
+    const card = document.createElement("li")
+    card.className = "cardCarrinho"
+    const img = document.createElement("img")
+    img.className = "imgCarrinho"
+    const info = document.createElement("div")
+    info.className = "infoCarrinho"
+    const button = document.createElement("img")
+    button.className = "buttonRemove"
+
+    const nome = document.createElement("h3")
+    const secao = document.createElement("span")
+    const preco = document.createElement("p")
+
+    img.src = item.img
+    button.src = './src/img/trash.png'
+    nome.innerText = item.nome
+    secao.innerText = item.secao
+    preco.innerText = `R$ ${item.preco}`
+
+    info.append(nome, secao, preco)
+    card.append(img, info, button)
+
+    ulCarrinho.appendChild(card)
+
+    return card
+}
+
+const addProduto = document.querySelectorAll(".addProduto")
+const remove = document.querySelectorAll(".buttonRemove")
+
+
+let precoContador = 0
+function carrinho(){
+    
+    for (let item of addProduto){
+        item.addEventListener("click", () => {
+            
+            precoContador += parseInt(produtos[item.id-1].preco)
+            
+            const card = criaCardCarrinho(produtos[item.id-1])
+            const buttonRemove = card.querySelector(".buttonRemove")
+            
+            precoTotal.innerText = `R$ ${precoContador}.00`
+            
+            buttonRemove.addEventListener("click", () => {
+                precoContador -= parseInt(produtos[item.id-1].preco)
+                ulCarrinho.removeChild(card)
+                precoTotal.innerText = `R$ ${precoContador}.00`
+            })
+        })       
+    }
+}
+
+carrinho()
+
 
 botaoBuscar.addEventListener("click", filtrarPorNome)
 
@@ -65,3 +151,4 @@ botaoMostrarTodos.addEventListener("click", () => {
     ul.innerHTML = ""
     posicionaCard(produtos)
 })
+
